80c4361d66fe60521d451ab7f55037599c3302dc
Justin Wark <justinw@justinw-laptop.(none)>
2012-01-24 22:19:05 +1100

    make compatible with rspec2/rails3.

--- Support/lib/cucumber/mate.rb
+++ Support/lib/cucumber/mate.rb
@@ -17,7 +17,6 @@ if ENV['TM_PROJECT_DIRECTORY']
     $LOAD_PATH.unshift(rspec_lib)
   end
 end
-require 'spec'
 
 $LOAD_PATH.unshift(File.expand_path(File.join(File.dirname(__FILE__), '..')))
 require "cucumber/mate/feature_helper"
